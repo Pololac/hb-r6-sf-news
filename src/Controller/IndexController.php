d17caf303d91b197c469b462816ee0b2899adcbe
Pololac <lacostepaul@yahoo.fr>
2024-08-05 16:08:31 +0200

    Ajout Tailwind

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class IndexController extends AbstractController
+{   //Une fonction par page
+    #[Route('/', name: 'homepage')]     // Attribut PHP8 : à mettre au-dessus de la f° concernée
+    public function index(): Response       // C'est le framework Symfony qui appelle les fonctions
+    {
+        return $this->render('index/index.html.twig', [
+            'user_name' => 'Polo',
+        ]);
+    }
+    
+    #[Route('/about', name: 'about')]
+    public function about(): Response
+    {
+        return $this->render('index/about.html.twig', []);
+    }
+}
